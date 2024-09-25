f684c130bdd430ab7767ea78c3882342c8d45393
ADMIN <ADMIN@PC239>
2024-09-25 11:12:57 +0700

    pb1

---
+++ src/main/java/model/QLSinhVien.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import util.FileHelper;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class QLSinhVien {
+    private ArrayList<SinhVien> dsSinhVien;
+    
+    public QLSinhVien() {
+        dsSinhVien = new ArrayList<>();        
+    }
+
+    public QLSinhVien(ArrayList<SinhVien> ds) {
+        this.dsSinhVien = ds;
+    }
+    
+    public ArrayList<SinhVien> getDsSinhVien() {
+        return dsSinhVien;
+    }
+
+    public void setDsSinhVien(ArrayList<SinhVien> dsSinhVien) {
+        this.dsSinhVien = dsSinhVien;
+    }
+    
+    public void DocDanhSachSinhVien(String filename)
+    {
+        //sinh viên viết code 
+        ArrayList<String> data = FileHelper.readFileText(filename); //doc file
+        //Đổ dữ liệu vào danh sách
+        dsSinhVien.clear();
+        for(String item : data)
+        {
+            String[] arr = item.split(";");
+            SinhVien sv = new SinhVien();
+            sv.setMaso(arr[0]);
+            sv.setHoten(arr[1]);
+            sv.setGioitinh(Boolean.parseBoolean(arr[2]));
+            sv.getDiemTB(Double.parseDouble(arr[3]));
+            
+        }
+    }
+
+    public boolean GhiDanhSachSinhVien(String filename)
+    {      
+        //sinh viên viết code 
+        return true;
+    }    
+    
+    public boolean themSV(SinhVien sv)
+    {
+       //sinh viên viết code 
+        return true;
+    }  
+    
+    public boolean xoaSV(String maso)
+    {
+        //sinh viên viết code 
+        return true;
+    }     
+   
+    public void sapXepTheoHocLuc()
+    {
+        //sinh viên viết code         
+    }   
+    
+}
---
+++ src/main/java/model/SinhVien.java
@@ -0,0 +1,84 @@
+package model;
+
+public class SinhVien {
+    private String maso;
+    private String hoten;
+    private boolean gioitinh;
+    private double diemTB;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String maso, String hoten, boolean gioitinh, double diemTB) {
+        this.maso = maso;
+        this.hoten = hoten;
+        this.gioitinh = gioitinh;
+        this.diemTB = diemTB;
+    }
+
+    public SinhVien(String maso) {
+        this.maso = maso;
+    }
+
+    public String getMaso() {
+        return maso;
+    }
+
+    public void setMaso(String maso) {
+        this.maso = maso;
+    }
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public boolean isGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(boolean gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public double getDiemTB() {
+        return diemTB;
+    }
+
+    public void setDiemTB(double diemTB) {
+        this.diemTB = diemTB;
+    }
+
+    public String getHocLuc()
+    {
+       String kq="";
+       //sinh viên viết code   
+        if(diemTB<5)
+       {
+          kq="Yếu";
+       }else if(diemTB<6.5)
+       {
+           kq="Trung bình";
+       }else if(diemTB <7.5)
+       {
+           kq="Khá";
+       }else if(diemTB<9)
+       {
+           kq="Giỏi";
+       }else
+       {
+           kq="Xuất sắc";
+       }       
+       return kq;
+    }  
+   public void displayInfo(){
+       System.out.println("Mã số: " + maso);
+       System.out.println("Họ tên: " + hoten);
+       System.out.println("Giới tính: " + (gioitinh ? "Nam" : "Nữ"));
+       System.out.println("Điểm trung bình: " + diemTB);
+       System.out.println("Học lực: " + getHocLuc());
+   }
+}
